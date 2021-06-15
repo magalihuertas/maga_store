9dbb59996b2cdeb9b6f4f3bf051db963530ab941
Magali Huertas <huertasmagali@gmail.com>
2021-06-15 16:28:16 -0300

    tercer commit

--- src/App.js
+++ src/App.js
@@ -10,18 +10,20 @@ import { Cards } from "./components/Cards";
 
 function App() {
   const product = [
-    { id: 1, nombre: "unik gato", precio: 10 },
-    { id: 2, nombre: "unik perro", precio: 20 }
+    { id: 1, nombre: "Unik Gatito 2 kilos", marca: "Unik Gato", precio: 10 },
+    { id: 2, nombre: "Unik Gatito 7,5 kilos", marca:"Unik Perro", precio: 20 }
   ];
   return (
     <div className="App">
       <header className="App-header">
-
         <NavBar />
         <Carrousel />
-        <Cards/>
+        <Cards productData ={product [0]} greeting={"Bienvenidos a la tienda"}/>
+        <Cards productData ={product [1]}/>
+        <br/>
+        <br/>
+        <br/>
         <Footer />
-        
         
         {/* <ItemListContainer greeting={"Bienvenidos a la tienda"}/> */}
         
--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './estilos.css';
 
-export const Cards = (props) => {
-    function agregarAlCarrito(){
+export const Cards = ({productData, greeting}) => {
+    function agregarAlCarrito() {
 
     }
     return (
         <div className="card">
-            <h1>Título de la card</h1>
-            <p>precio del producto</p>
+            <h1>{productData.nombre}</h1>
+            <p>{productData.marca}</p>
+            <p>{productData.precio}</p>
+            <p>{greeting}</p>
+
             <button onClick={agregarAlCarrito}>Agregar al Carrito</button>
         </div>
     )
