d6fcc64003c55fea210d611db4d11ba060a64319
Magali Huertas <huertasmagali@gmail.com>
2021-06-26 14:44:20 -0300

    consumiendo api

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -11,7 +11,6 @@ export const Item = ({productData,onAdd,onSubstract}) => {
             <h1>{productData.nombre}</h1>
             <img src={productData.imagen} alt="imagen"/>
             <p>{productData.precio}</p>
-            
             <ItemCount stock={productData.stock} initial={1} onAdd={onAdd} onSubstract={onSubstract}/>
             <button onClick={agregarAlCarrito}>Agregar al Carrito</button>
         </div>
